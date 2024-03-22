aeafa85ab6cb7d482f74280632baaba8b68b560c
Michael <magee.michael96@gmail.com>
2024-03-22 16:06:16 +0200

    added empty springboot project

---
+++ gptgenerator/src/main/java/com/gptool/gptgenerator/GptgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.gptool.gptgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptgeneratorApplication.class, args);
+	}
+
+}
