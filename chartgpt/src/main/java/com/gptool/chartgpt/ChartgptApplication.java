7f4e42a8434ec1406b8fda8b251dd7d99efede06
Michael <magee.michael96@gmail.com>
2024-04-06 17:00:03 +0200

    added empty spring_boot project

---
+++ chartgpt/src/main/java/com/gptool/chartgpt/ChartgptApplication.java
@@ -0,0 +1,13 @@
+package com.gptool.chartgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartgptApplication.class, args);
+	}
+
+}
